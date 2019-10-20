4c78a8630e4f13c5e01cd27f98e5d2441f116c33
ArranHL <47016295+ArranHL@users.noreply.github.com>
2019-10-20 15:30:52 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,3 @@
-variable "notcount" {
-  default = 1
-}
-
-variable "key_name" {
-  description = "AWS key"
-  default     = "TerraformJenkins20190930"
-}
-
-
 
   variable "github_organization" {
   description = "Organization Name"
@@ -20,14 +10,6 @@ variable "TOKEN" {
    default     = "vault-github-access-token"
 }
 
-variable "instance_type" {
-  description = "AWS instance type"
-  default     = "t3.small"
-}
 
-variable "ami" {
-  description = "Base AMI to launch the instances"
 
-  # Bitnami NGINX AMI
-  default = "ami-0e9c8f3f51f2984e6"
-}
+
